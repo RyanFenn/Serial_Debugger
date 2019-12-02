0381c40c5ea922ecd77b96173a3786bebee1fb36
RyanFenn <40464847+RyanFenn@users.noreply.github.com>
2019-12-02 12:46:02 -0500

    Create simpleSerialDebug.ino
    File to test the library

---
+++ examples/simpleSerialDebug/simpleSerialDebug.ino
@@ -0,0 +1,18 @@
+
+#define DEBUG //when defined, it prints messages  //Needs to be defined before #include "Debug.h"
+#include "Debug.h" 
+
+unsigned char myCounter; 
+
+void setup() {
+  DEBUG_BEGIN(115200); 
+  DEBUG_PRINTLN("Hello World");
+  myCounter = 0;  
+}
+
+void loop() {
+
+  DEBUG_PRINT("myCounter = "); DEBUG_PRINTLN(myCounter); 
+  myCounter++; 
+  delay(1000); 
+}
